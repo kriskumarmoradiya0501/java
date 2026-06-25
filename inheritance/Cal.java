9d066a1d8f3f923b6f6c31db7939637c4185751b
krish moradiya <140172827+kriskumarmoradiya0501@users.noreply.github.com>
2026-06-25 11:27:40 +0530

    Create Cal.java

---
+++ inheritance/Cal.java
@@ -0,0 +1,13 @@
+class Cal{
+    public static void main(String[] args){
+        AddSub as = new AddSub();
+
+        float result = as.add(10,20);
+        System.out.println(result);
+        
+        MulDiv md = new MulDiv();
+        result = md.add(20, 30);
+
+        System.out.println(result);
+    }
+}
