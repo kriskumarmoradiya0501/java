ceda9f0f000e35cd36cc1c9ee7962ad85cb4e196
krish moradiya <140172827+kriskumarmoradiya0501@users.noreply.github.com>
2026-06-25 11:28:19 +0530

    Add files via upload

---
+++ inheritance/AddSub.java
@@ -0,0 +1,8 @@
+class AddSub{
+    public int add(int a,int b){
+        return a+b;
+    }
+    public int sub(int a,int b){
+        return a-b;
+    }
+}
---
+++ inheritance/MulDiv.java
@@ -0,0 +1,8 @@
+class MulDiv extends AddSub{
+    public float mul(int a,int b){
+        return a*b;
+    }
+    public float dic(int a,int b){
+        return a/b;
+    }
+}
